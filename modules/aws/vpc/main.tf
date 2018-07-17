74e5575d80a5b33b644f878c1637f710bc0076a4
keitakn <keita.koga.work@gmail.com>
2018-07-17 16:59:25 +0900

    :sparkles: #2 public用のサブネットとインターネットGatewayを追加

--- modules/aws/vpc/main.tf
+++ modules/aws/vpc/main.tf
@@ -7,3 +7,41 @@ resource "aws_vpc" "vpc" {
     Name = "${terraform.workspace}-${lookup(var.common, "${terraform.env}.project", var.common["default.project"])}-vpc"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  tags {
+    Name = "${terraform.workspace}-igw"
+  }
+}
+
+resource "aws_subnet" "public_1a" {
+  vpc_id            = "${aws_vpc.vpc.id}"
+  cidr_block        = "${lookup(var.vpc, "${terraform.env}.public_1a", var.vpc["default.public_1a"])}"
+  availability_zone = "${lookup(var.common, "${terraform.env}.region", var.common["default.region"])}a"
+
+  tags {
+    Name = "${terraform.workspace}-public-1a"
+  }
+}
+
+resource "aws_subnet" "public_1c" {
+  vpc_id            = "${aws_vpc.vpc.id}"
+  cidr_block        = "${lookup(var.vpc, "${terraform.env}.public_1c", var.vpc["default.public_1c"])}"
+  availability_zone = "${lookup(var.common, "${terraform.env}.region", var.common["default.region"])}c"
+
+  tags {
+    Name = "${terraform.workspace}-public-1c"
+  }
+}
+
+resource "aws_subnet" "public_1d" {
+  vpc_id            = "${aws_vpc.vpc.id}"
+  cidr_block        = "${lookup(var.vpc, "${terraform.env}.public_1d", var.vpc["default.public_1d"])}"
+  availability_zone = "${lookup(var.common, "${terraform.env}.region", var.common["default.region"])}d"
+
+  tags {
+    Name = "${terraform.workspace}-public-1d"
+  }
+}
--- providers/aws/variable.tf
+++ providers/aws/variable.tf
@@ -11,9 +11,21 @@ variable "vpc" {
   type = "map"
 
   default = {
-    default.cidr = "10.0.0.0/16"
-    stg.cidr     = "10.1.0.0/16"
-    dev.cidr     = "10.2.0.0/16"
-    qa.cidr      = "10.3.0.0/16"
+    default.cidr      = "10.0.0.0/16"
+    stg.cidr          = "10.1.0.0/16"
+    dev.cidr          = "10.2.0.0/16"
+    qa.cidr           = "10.3.0.0/16"
+    default.public_1a = "10.0.1.0/26"
+    default.public_1c = "10.0.1.64/26"
+    default.public_1d = "10.0.1.128/26"
+    stg.public_1a     = "10.1.1.0/26"
+    stg.public_1c     = "10.1.1.64/26"
+    stg.public_1d     = "10.1.1.128/26"
+    dev.public_1a     = "10.2.1.0/26"
+    dev.public_1c     = "10.2.1.64/26"
+    dev.public_1d     = "10.2.1.128/26"
+    qa.public_1a      = "10.3.1.0/26"
+    qa.public_1c      = "10.3.1.64/26"
+    qa.public_1d      = "10.3.1.128/26"
   }
 }

75bd0b1dcfd09c52061539bd674e8bf1573571a8
keitakn <keita.koga.work@gmail.com>
2018-07-17 18:59:19 +0900

    :recycle: #2 アプトプットに各サブネットIDを追加

--- modules/aws/vpc/output.tf
+++ modules/aws/vpc/output.tf
@@ -1,7 +1,10 @@
 output "vpc" {
   value = "${
     map(
-      "vpc_id", "${aws_vpc.vpc.id}"
+      "vpc_id", "${aws_vpc.vpc.id}",
+      "subnet_public_1a", "${aws_subnet.public_1a.id}",
+      "subnet_public_1c", "${aws_subnet.public_1c.id}",
+      "subnet_public_1d", "${aws_subnet.public_1d.id}"
     )
   }"
 }

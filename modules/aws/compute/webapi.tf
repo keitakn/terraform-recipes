9693ed03040e93b81179537cafa9df6e9edf69f7
keitakn <keita.koga.work@gmail.com>
2018-07-22 00:24:50 +0900

    :sparkles: #5 SSH接続が出来るようにセキュリティグループの設定を追加

--- modules/aws/compute/webapi.tf
+++ modules/aws/compute/webapi.tf
@@ -14,3 +14,12 @@ resource "aws_security_group" "webapi" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group_rule" "ssh_from_bastion" {
+  security_group_id        = "${aws_security_group.webapi.id}"
+  type                     = "ingress"
+  from_port                = "22"
+  to_port                  = "22"
+  protocol                 = "tcp"
+  source_security_group_id = "${aws_security_group.bastion.id}"
+}

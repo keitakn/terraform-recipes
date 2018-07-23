b6a30fbee6d028fe541b1de198887e42d8f55d55
keitakn <keita.koga.work@gmail.com>
2018-07-23 22:37:47 +0900

    :sparkles: #14 データベースのmasterユーザーとパスワードを表す変数を追加

--- modules/aws/rds/variable.tf
+++ modules/aws/rds/variable.tf
@@ -15,3 +15,13 @@ variable "vpc" {
 
   default = {}
 }
+
+variable "rds_master_username" {
+  type    = "string"
+  default = ""
+}
+
+variable "rds_master_password" {
+  type    = "string"
+  default = ""
+}

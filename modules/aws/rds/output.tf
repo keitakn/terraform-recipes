87b0b6c57ce26a54129b4f72864bcbdf099e3c74
keitakn <keita.koga.work@gmail.com>
2018-07-24 19:00:22 +0900

    :sparkles: #17 RDS用のローカルドメインをアウトプットに追加

--- modules/aws/rds/output.tf
+++ modules/aws/rds/output.tf
@@ -3,7 +3,8 @@ output "rds" {
     map(
       "rds_subnet_group_id", "${aws_db_subnet_group.rds_subnet.id}",
       "endpoint", "${aws_rds_cluster.rds_cluster.endpoint}",
-      "reader_endpoint", "${aws_rds_cluster.rds_cluster.reader_endpoint}"
+      "reader_endpoint", "${aws_rds_cluster.rds_cluster.reader_endpoint}",
+      "rds_local_domain_name", "${aws_route53_zone.rds_local_domain_name.name}"
     )
   }"
 }

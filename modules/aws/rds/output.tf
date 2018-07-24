6e3294ff28fbcbd4fbb66dbbb9500d6fce403189
keitakn <keita.koga.work@gmail.com>
2018-07-24 16:51:35 +0900

    :sparkles: #17 RDSのclusterエンドポイントをアウトプットに追加

--- modules/aws/rds/output.tf
+++ modules/aws/rds/output.tf
@@ -1,7 +1,9 @@
 output "rds" {
   value = "${
     map(
-      "rds_subnet_group_id", "${aws_db_subnet_group.rds_subnet.id}"
+      "rds_subnet_group_id", "${aws_db_subnet_group.rds_subnet.id}",
+      "endpoint", "${aws_rds_cluster.rds_cluster.endpoint}",
+      "reader_endpoint", "${aws_rds_cluster.rds_cluster.reader_endpoint}"
     )
   }"
 }

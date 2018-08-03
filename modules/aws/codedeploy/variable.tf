1bd419a0655eb96f36ccf6d56caabb762f4285a4
keitakn <keita.koga.work@gmail.com>
2018-08-04 01:35:29 +0900

    :sparkles: #27 Webフロントエンド用のCodeDeployアプリケーションを追加

--- modules/aws/codedeploy/variable.tf
+++ modules/aws/codedeploy/variable.tf
@@ -1,3 +1,15 @@
+variable "web_codedeploy_app_name" {
+  type = "string"
+
+  default = ""
+}
+
+variable "web_deploy_bucket_name" {
+  type = "string"
+
+  default = ""
+}
+
 variable "webapi_codedeploy_app_name" {
   type = "string"
 
